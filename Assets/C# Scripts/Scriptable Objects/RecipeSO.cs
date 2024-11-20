879cae3acbed33cf305f9474d1e9d9edb0a4053a
FirePixel8422 <bmtminecraft@hotmail.com>
2024-11-20 13:26:43 +0100

    Food System Patch

--- Assets/C# Scripts/Scriptable Objects/RecipeSO.cs
+++ Assets/C# Scripts/Scriptable Objects/RecipeSO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(fileName = "New Recipe", menuName = "Cooking System/Food/Recipe", order = 0)]
+[CreateAssetMenu(fileName = "New Recipe", menuName = "Cooking System/Recipe", order = 0)]
 public class RecipeSO : ScriptableObject
 {
     public Recipe recipe;
